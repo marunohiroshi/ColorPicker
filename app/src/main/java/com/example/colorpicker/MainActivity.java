0414c778ca6195e6dc1f468d4ec3af2db0190396
maruno hiroshi <hiro.0405.maru@gmail.com>
2019-10-24 23:53:24 +0900

    modify files

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.colorpicker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.Serializable;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity  {
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+
+public class MainActivity extends AppCompatActivity {
 
     TextView redColorStrong;
     TextView greenColorStrong;
     TextView blueColorStrong;
+    ImageView imageView;
 
 
     @Override
@@ -26,10 +38,27 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Title");
+        alert.setMessage("Message");
+        alert.setPositiveButton("OK", null);
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Yesボタンが押された時の処理
+                Toast.makeText(MainActivity.this, "Yes Clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Noボタンが押された時の処理
+                Toast.makeText(MainActivity.this, "No Clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
+
         SeekBar redSeekBar = findViewById(R.id.red_seek_bar);
         SeekBar greenSeekBar = findViewById(R.id.green_seek_bar);
         SeekBar blueSeekBar = findViewById(R.id.blue_seek_bar);
-        final ImageView imageView = findViewById(R.id.imageView);
 
         //最大値と最小値を設定
         redSeekBar.setMax(255);
@@ -42,9 +71,10 @@ public class MainActivity extends AppCompatActivity  {
         redColorStrong = findViewById(R.id.red_color_strong);
         greenColorStrong = findViewById(R.id.green_color_strong);
         blueColorStrong = findViewById(R.id.blue_color_strong);
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.display_button);
 
-        View display_button = findViewById(R.id.display_button);
-        display_button.setOnClickListener(display_button_onClickListener);
+        button.setOnClickListener(buttonClick);
 
         redSeekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
@@ -52,6 +82,26 @@ public class MainActivity extends AppCompatActivity  {
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         String redstr = String.format(Locale.US, "%d", progress);
                         redColorStrong.setText(redstr);
+                    }
+
+                    //つまみがタッチされたときに呼ばれるメソッド
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    //つまみがリリースされたときに呼ばれるメソッド
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+        );
+
+        greenSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        String redstr = String.format(Locale.US, "%d", progress);
+                        greenColorStrong.setText(redstr);
                     }
 
                     //つまみがタッチされたときに呼ばれるメソッド
@@ -86,52 +136,26 @@ public class MainActivity extends AppCompatActivity  {
                 }
         );
 
-        greenSeekBar.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        String bluestr = String.format(Locale.US, "%d", progress);
-                        greenColorStrong.setText(bluestr);
-                    }
+        button.setBackgroundColor(Color.rgb(100, 100, 100));
 
-                    //つまみがタッチされたときに呼ばれるメソッド
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-                    }
+//        View.OnClickListener display_button_onClickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        };
+    }
 
-                    //つまみがリリースされたときに呼ばれるメソッド
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-                    }
-                }
-        );
-
-        public void paint (Graphics g){
-            Color color = new Color(redstr, greenstr, bluestr )
-                    g.setColor(color);
-        }
-
-        imageView.setTextColor(Color.rgb(redstr,  greenstr,  bluestr));
-
+    public OnClickListener buttonClick = new OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch(v.getId()){
-                case R.id.display_button:
-                    Log.d("My Test", "Clicked!");
-            }
+            Log.d("debug", "button, Perform action on click");
+
         }
+    };
+
+    private void setScreenSub() {
+        setContentView(R.layout.dialog_fragment);
     }
+}
 
-
-        View.OnClickListener display_button_onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //色表示ボタンが押されたときの挙動
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("EXTRA_DATA",  imageView.getText().toString());//渡すデータの名前と値を入れる
-                startActivity(intent);
-            }
-        };
-
-    }
 
