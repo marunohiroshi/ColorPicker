6af98d1c7c5695004d335bae70eb61a9b9917fed
Maruno <hiro.0405.maru@gmail.com>
2020-01-05 23:16:15 +0900

    Modify

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -15,15 +15,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView redColorStrong;
     private TextView greenColorStrong;
     private TextView blueColorStrong;
-    private static int redProgress;
-    private static int greenProgress;
-    private static int blueProgress;
-    public static String redStr;
-    public static String greenStr;
-    public static String blueStr;
-    public TextView fragmentRedStr;
-    public TextView fragmentGreenStr;
-    public TextView fragmentBlueStr;
+    private int redProgress;
+    private int greenProgress;
+    private int blueProgress;
+    public  String redStr;
+    public  String greenStr;
+    public  String blueStr;
     public TextView fragment;
 
 
@@ -35,15 +32,11 @@ public class MainActivity extends AppCompatActivity {
         final SeekBar redSeekBar = findViewById(R.id.red_seek_bar);
         final SeekBar greenSeekBar = findViewById(R.id.green_seek_bar);
         final SeekBar blueSeekBar = findViewById(R.id.blue_seek_bar);
-        fragmentRedStr = findViewById(R.id.leftColorStrong);
-        fragmentGreenStr = findViewById(R.id.middleColorStrong);
-        fragmentBlueStr = findViewById(R.id.rightColorStrong);
         fragment = findViewById(R.id.fragment);
         redColorStrong = findViewById(R.id.red_color_strong);
         greenColorStrong = findViewById(R.id.green_color_strong);
         blueColorStrong = findViewById(R.id.blue_color_strong);
         Button button = findViewById(R.id.display_button);
-
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showFragmentDialog() {
-        TestDialogFragment dialogFragment = TestDialogFragment.newInstance(redProgress, greenProgress, blueProgress);
+        TestDialogFragment dialogFragment = TestDialogFragment.newInstance(redProgress, greenProgress, blueProgress, redStr, greenStr, blueStr);
         dialogFragment.show(getSupportFragmentManager(), "dialog_fragment");
     }
 }
--- app/src/main/java/com/example/colorpicker/TestDialogFragment.java
+++ app/src/main/java/com/example/colorpicker/TestDialogFragment.java
@@ -10,19 +10,26 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
-public class TestDialogFragment extends androidx.fragment.app.DialogFragment {
-    private final static String redKey = "KEY_RED";
-    private final static String greenKey = "KEY_GREEN";
-    private final static String blueKey = "KEY_BLUE";
+public class TestDialogFragment extends DialogFragment {
+    private static final String redIntKey = "redIntKey";
+    private static final String greenIntKey = "greenIntKey";
+    private static final String blueIntKey = "blueIntKey";
+    private static final String redStringKey = "redStringKey";
+    private static final String greenStringKey = "greenStringKey";
+    private static final String blueStringKey = "blueStringKey";
 
 
-    static TestDialogFragment newInstance(int red, int green, int blue) {
+    static TestDialogFragment newInstance(int red, int green, int blue, String redstr, String greenstr, String bluestr) {
         TestDialogFragment tdf = new TestDialogFragment();
         Bundle bundle = new Bundle();
-        bundle.putInt(redKey, red);
-        bundle.putInt(greenKey, green);
-        bundle.putInt(blueKey, blue);
+        bundle.putInt(redIntKey, red);
+        bundle.putInt(greenIntKey, green);
+        bundle.putInt(blueIntKey, blue);
+        bundle.putString(redStringKey, redstr);
+        bundle.putString(greenStringKey, greenstr);
+        bundle.putString(blueStringKey, bluestr);
         tdf.setArguments(bundle);
         return tdf;
     }
@@ -41,29 +48,32 @@ public class TestDialogFragment extends androidx.fragment.app.DialogFragment {
     @Override
     public void onStart() {
         super.onStart();
-        int redInt = getArguments().getInt(redKey);
-        int GreenInt = getArguments().getInt(greenKey);
-        int BlueInt = getArguments().getInt(blueKey);
+        int redInt = getArguments().getInt(redIntKey);
+        int greenInt = getArguments().getInt(greenIntKey);
+        int blueInt = getArguments().getInt(blueIntKey);
+        String redString = getArguments().getString(redStringKey);
+        String greenString = getArguments().getString(greenStringKey);
+        String blueString = getArguments().getString(blueStringKey);
         TextView fragment = getDialog().findViewById(R.id.fragment);
         TextView fragmentRedStr = getDialog().findViewById(R.id.leftColorStrong);
         TextView fragmentGreenStr = getDialog().findViewById(R.id.middleColorStrong);
         TextView fragmentBlueStr = getDialog().findViewById(R.id.rightColorStrong);
-        fragment.setBackgroundColor(Color.rgb(redInt, GreenInt, BlueInt));
+        fragment.setBackgroundColor(Color.rgb(redInt, greenInt, blueInt));
 
         if (redInt == 0) {
             fragmentRedStr.setText("0");
         } else {
-            fragmentRedStr.setText(MainActivity.redStr);
+            fragmentRedStr.setText(redString);
         }
-        if (GreenInt == 0) {
+        if (greenInt == 0) {
             fragmentGreenStr.setText("0");
         } else {
-            fragmentGreenStr.setText(MainActivity.greenStr);
+            fragmentGreenStr.setText(greenString);
         }
-        if (BlueInt == 0) {
+        if (blueInt == 0) {
             fragmentBlueStr.setText("0");
         } else {
-            fragmentBlueStr.setText(MainActivity.blueStr);
+            fragmentBlueStr.setText(blueString);
         }
     }
 }
